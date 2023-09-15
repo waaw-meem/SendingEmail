70216657022c7518b567b7607b61ea9dd2a04e11
Wali Muhammad <wm401238@gmail.com>
2023-09-15 15:21:39 +0500

    Validation in progress

--- routes/auth.js
+++ routes/auth.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const { check, body } = require('express-validator')
 
 const authController = require('../controllers/auth');
 
@@ -10,7 +11,28 @@ router.get('/signup', authController.getSignup);
 
 router.post('/login', authController.postLogin);
 
-router.post('/signup', authController.postSignup);
+router.post('/signup', 
+[
+check('email')
+.isEmail()
+.withMessage('Please Enter a valid Email Address')
+.custom((value,{req}) => {
+    if(value === 'test@test.com'){
+        throw new Error('This Email address is forbidden')
+    }
+    return true
+}),
+body('password',"Please Enter a valid Password at least 5 characters")
+.isLength({min:5,max:17})
+.isAlphanumeric(),
+body('confirmPassword').custom((value, {req}) => {
+    if(value !== req.body.password){
+        throw new Error("Your Password should me match!")
+    }
+    return true
+})
+]
+, authController.postSignup);
 
 router.post('/logout', authController.postLogout);
 
